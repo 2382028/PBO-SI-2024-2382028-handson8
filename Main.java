d1321db387ef5578068102b6f3f60ad27ac45dbd
2382028 <2382028@unai.edu>
2024-11-12 10:24:13 +0700

    komit well 2

--- Main.java
+++ Main.java
@@ -1,16 +1,21 @@
 import Repositories.TodoListRepository;
+import Repositories.TodoListRepositoryDbImpl;
 import Repositories.TodoListRepositoryImpl;
 import Services.TodoListService;
 import Services.TodoListServiceImpl;
 import Views.TodoListTerminalView;
 import Views.TodoListView;
+import config.Database;
 
 
 public class Main {
     public static void main(String[] args) {
-       TodoListRepository todoListRepository = new TodoListRepositoryImpl();
-        TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
-        TodoListView todoListView = new TodoListTerminalView(todoListService);
-       todoListView.run();
+        Database database = new Database("databaseku", "root", "", "localhost", "3306");
+        database.setup();
+     TodoListRepository todoListRepository = new TodoListRepositoryDbImpl();
+
+     TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
+     TodoListView todoListView = new TodoListTerminalView(todoListService);
+     todoListView.run();
     }
 }
---
+++ Repositories/TodoListRepositoryDbImpl.java
@@ -0,0 +1,54 @@
+package Repositories;
+
+import Entities.TodoList;
+import config.Database;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TodoListRepositoryDbImpl implements TodoListRepository{
+    private final Database database;
+
+    public TodoListRepositoryDbImpl(){
+        this.database = database;
+    }
+
+    @Override
+    public TodoList[] getAll() {
+        Connection connection = database.getConnection();
+        String sqlStatement = "SELECT * FROM todos";
+        List<TodoList> todoLists = new ArrayList<>();
+        try{
+            PreparedStatement preparedStatement = connection.prepareStatement(sqlStatement);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                TodoList todoList = new TodoList();
+                int id = resultSet.getInt(1);
+                String todo = resultSet.getString(2);
+                todoList.setId(id);
+                todoList.setTodo(todo);
+                todoLists.add(todoList);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return todoLists.toArray(TodoList[]::new);
+    }
+
+    @Override
+    public void add(TodoList todoList) {
+
+    }
+
+    @Override
+    public Boolean remove(Integer id) {
+        return null;
+    }
+
+    @Override
+    public Boolean edit(TodoList todoList) {
+        return null;
+    }
+}
